69aa532b67ec55230b4160e391d0705d808762a2
Flacko <f@ak68a.co>
2023-06-05 17:33:39 -0400

    first commit

---
+++ Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Ayo Moreira on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
